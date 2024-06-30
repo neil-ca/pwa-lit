93537572abc24cf4a83ef917be5aff010a62e031
Neil Campos <ulicode4@gmail.com>
2024-06-30 11:52:09 -0600

    post test with fake data pass

--- server/__tests__/products.test.js
+++ server/__tests__/products.test.js
@@ -4,10 +4,22 @@ const { app } = require('../src/server')
 
 describe('POST /products', () => {
   test('should store a new product', async () => {
-    await request(app)
+    const response = await request(app)
       .post('/products')
+      .send({
+        name: 'my product',
+        description: 'this is a test',
+        price: '190',
+      })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(201)
+
+    expect(response.body).toEqual({
+      name: 'my product',
+      description: 'this is a test',
+      price: '190',
+      _id: 'abc',
+    })
   })
 })
--- server/src/server.js
+++ server/src/server.js
@@ -4,11 +4,22 @@ const app = express()
 
 const port = 8081
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
 app.post('/products', (req, res) => {
-  res.status(201).json()
+  const { name, description, price } = req.body
+  const _id = 'abc'
+
+  res.status(201).json({
+    name,
+    description,
+    price,
+    _id,
+  })
 })
 
 if (require.main === module) {
